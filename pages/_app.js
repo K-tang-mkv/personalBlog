c0616e795bdbabe5618c6d3dc3b1135e90e61fa2
fuck <sellintang@Sellins-MacBook-Air.local>
2021-10-21 02:16:09 +0800

    blog init

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import '../styles/globals.css'
+import Link from 'next/link'
+function MyApp({ Component, pageProps }) {
+
+  return (
+    <div className="mx-auto my-8 w-9/12">
+      <header>
+        <h1 className="text-6xl font-bold text-center">Cole Blog</h1>
+        <nav className="my-4">
+         
+          <ul className="flex flex-row justify-center space-x-4">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      
+      <Component {...pageProps} />
+    </div>
+    
+  )
+}
+
+export default MyApp
